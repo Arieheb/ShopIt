ed7326af37263cf33cce88db56f5a42f8d8870b7
Arieh Berlin <Arieh.berlin@gmail.com>
2023-05-28 21:31:10 +0300

    3 dot menu addition

--- app/src/main/java/com/ariehb_miriams/shopit/HomeScreenActivity.java
+++ app/src/main/java/com/ariehb_miriams/shopit/HomeScreenActivity.java
@@ -1,8 +1,15 @@
 package com.ariehb_miriams.shopit;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class HomeScreenActivity extends AppCompatActivity {
 
@@ -10,5 +17,89 @@ public class HomeScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        super.onCreateOptionsMenu(menu);
+
+        MenuItem aboutMenu = menu.add("About");
+        MenuItem settingsMenu = menu.add("Settings");
+        MenuItem exitMenu = menu.add("Exit");
+
+        settingsMenu.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(@NonNull MenuItem menuItem) {
+                Intent intent = new Intent(HomeScreenActivity.this, ProfileActivity.class);
+                startActivity(intent);
+                return false;
+            }
+        });
+
+        aboutMenu.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item)
+            {
+                aboutAlertDialog();
+                return false;
+            }
+        });
+
+        exitMenu.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item)
+            {
+                exitAlertDialog();
+                return false;
+            }
+        });
+
+        return true;
+    }
+
+
+    private void aboutAlertDialog()
+    {
+        String strDeviceOS = "Android OS " + Build.VERSION.RELEASE + " API " + Build.VERSION.SDK_INT;
+
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("About App");
+        dialog.setMessage("\nShopit is a social shopping list app" + "\n\n" + strDeviceOS + "\n\n" + "By Arieh Berlin & Miriam Sirota 2023.");
+
+        dialog.setPositiveButton("OK", new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                dialog.dismiss();   // close this dialog
+            }
+        });
+        dialog.show();
+    }
+    private void exitAlertDialog()
+    {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+//        dialog.setIcon(R.drawable.icon_exit);
+        dialog.setTitle("Exit App");
+        dialog.setMessage("Are you sure ?");
+        dialog.setCancelable(false);
+
+        dialog.setPositiveButton("YES", new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                finish();   // destroy this activity
+            }
+        });
+        dialog.setNegativeButton("NO", new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                dialog.dismiss();   // close this dialog
+            }
+        });
+        dialog.show();
     }
 }
