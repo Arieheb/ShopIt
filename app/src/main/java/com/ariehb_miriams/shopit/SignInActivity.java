a381be73a04fb7f02c74037c611bf6d9d0c7a34e
Arieh Berlin <Arieh.berlin@gmail.com>
2023-05-31 14:54:12 +0300

    sign up <-> sign in navigation

--- app/src/main/java/com/ariehb_miriams/shopit/SignInActivity.java
+++ app/src/main/java/com/ariehb_miriams/shopit/SignInActivity.java
@@ -2,7 +2,12 @@ package com.ariehb_miriams.shopit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.view.View;
+import android.widget.TextView;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -10,5 +15,21 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        TextView linkText = findViewById(R.id.textView9);
+
+
+        linkText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an Intent to navigate to the new activity
+                Intent intent = new Intent(SignInActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
     }
 }
